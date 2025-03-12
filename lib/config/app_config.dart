dd9070595042ca68d060653346f8ed646d5607da
Salvad0r <56884312+salvatore-esposito-green@users.noreply.github.com>
2025-03-12 12:33:43 +0100

    Refactor SettingsScreen to StatefulWidget; add customization options for app name, logo, and primary color

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class AppConfig {
-  static const String appName = "Cold River Express";
-  static const String logoPath = "assets/icons/icon-appbar.png";
-  static const String dateLocal = "it_IT";
-  static const Color primaryColor = Colors.blue;
+  static String appName = "Cold River Express";
+  static String logoPath = "assets/icons/icon-appbar.png";
+  static String dateLocal = "it_IT";
+  static Color primaryColor = Colors.blue;
 }
--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,11 +1,24 @@
+import 'dart:io';
+
 import 'package:cold_river_express_app/config/app_config.dart';
 import 'package:flutter/material.dart';
 import 'package:cold_river_express_app/services/file_service.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
+  const SettingsScreen({super.key});
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
   final FileService _fileService = FileService();
 
-  SettingsScreen({super.key});
+  final TextEditingController _appNameController = TextEditingController(
+    text: AppConfig.appName,
+  );
 
   Future<void> shareDatabase(BuildContext context) async {
     final sharedRes = await _fileService.shareDatabase();
@@ -26,6 +39,77 @@ class SettingsScreen extends StatelessWidget {
         duration: Duration(seconds: 2),
       ),
     );
+  }
+
+  Future<void> _pickNewLogo() async {
+    final ImagePicker picker = ImagePicker();
+    final XFile? pickedFile = await picker.pickImage(
+      source: ImageSource.gallery,
+    );
+    if (pickedFile != null) {
+      setState(() {
+        AppConfig.logoPath = pickedFile.path;
+      });
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Logo changed successfully!'),
+          duration: Duration(seconds: 2),
+        ),
+      );
+    }
+  }
+
+  Future<void> _pickPrimaryColor() async {
+    Color selectedColor = AppConfig.primaryColor;
+
+    await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Select Primary Color'),
+          content: SingleChildScrollView(
+            child: ColorPicker(
+              pickerColor: selectedColor,
+              onColorChanged: (Color color) {
+                selectedColor = color;
+              },
+              pickerAreaHeightPercent: 0.8,
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            ElevatedButton(
+              child: const Text('Select'),
+              onPressed: () {
+                setState(() {
+                  AppConfig.primaryColor = selectedColor;
+                });
+
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text('Primary color changed successfully!'),
+                    duration: Duration(seconds: 2),
+                  ),
+                );
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _appNameController.dispose();
+    super.dispose();
   }
 
   @override
@@ -49,6 +133,54 @@ class SettingsScreen extends StatelessWidget {
               onTap: () {
                 shareDatabase(context);
               },
+            ),
+            const SizedBox(height: 16),
+            const Divider(),
+            const SizedBox(height: 16),
+            const Text(
+              'Customize',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 16),
+            TextField(
+              controller: _appNameController,
+              decoration: const InputDecoration(
+                labelText: 'App Name',
+                border: OutlineInputBorder(),
+              ),
+              onChanged: (value) {
+                setState(() {
+                  AppConfig.appName = value;
+                });
+              },
+            ),
+            const SizedBox(height: 16),
+            Row(
+              children: [
+                Text('Logo:', style: Theme.of(context).textTheme.bodyLarge),
+                const SizedBox(width: 16),
+                Image.asset(
+                  AppConfig.logoPath,
+                  height: 50,
+                  width: 50,
+                  errorBuilder: (context, error, stackTrace) {
+                    return Image.file(
+                      File(AppConfig.logoPath),
+                      height: 50,
+                      width: 50,
+                    );
+                  },
+                ),
+                const SizedBox(width: 16),
+                IconButton(icon: Icon(Icons.edit), onPressed: _pickNewLogo),
+              ],
+            ),
+            const SizedBox(height: 16),
+            ListTile(
+              leading: const Icon(Icons.color_lens),
+              title: const Text('Change Primary Color'),
+              subtitle: const Text('Select a new primary color for the app.'),
+              onTap: _pickPrimaryColor,
             ),
           ],
         ),
